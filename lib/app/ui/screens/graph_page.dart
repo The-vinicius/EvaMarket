54ca4c6572c9e572f9e3489218ad93cadf71fc5d
The-vinicuis <viniciusnao41@gmail.com>
2024-12-26 10:54:38 -0300

    close graph ui

--- lib/app/ui/screens/graph_page.dart
+++ lib/app/ui/screens/graph_page.dart
@@ -1,3 +1,4 @@
+import 'package:evamarket/app/models/chart_data.dart';
 import 'package:evamarket/app/models/ticker_model.dart';
 import 'package:evamarket/app/ui/states/data_state.dart';
 import 'package:flutter/material.dart';
@@ -10,7 +11,6 @@ class GraphPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final ticker = ModalRoute.of(context)!.settings.arguments as TickerModel;
-    final now = DateTime.now();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Graph Screen'),
@@ -18,139 +18,113 @@ class GraphPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 10, bottom: 3, left: 5),
-                child: Text(
-                  "${stocks.assetModel?.prices.last.toStringAsFixed(2)}",
-                  style: const TextStyle(
-                      fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5),
-                child: Text(
-                  ticker.name,
-                  style: const TextStyle(color: Colors.grey),
-                ),
-              ),
-              ListenableBuilder(
-                listenable: stocks,
-                builder: (context, w) {
-                  if (stocks.loading) {
-                    return const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: LinearProgressIndicator(),
-                    );
-                  } else if (stocks.assetModel == null || stocks.error) {
-                    return const Center(
+          child: ListenableBuilder(
+            listenable: stocks,
+            builder: (context, w) {
+              if (stocks.loading) {
+                return const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: LinearProgressIndicator(),
+                );
+              } else if (stocks.assetModel == null || stocks.error) {
+                return const Center(
+                  child: Text(
+                    'Sem Dados',
+                    key: Key('Sem'),
+                  ),
+                );
+              } else {
+                final chartData = stocks.data;
+                final mim = stocks.minPrice();
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(top: 10, bottom: 3, left: 5),
                       child: Text(
-                        'Sem Dados',
-                        key: Key('Sem'),
+                        "${stocks.assetModel?.prices.last.toStringAsFixed(2)}",
+                        style: const TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
                       ),
-                    );
-                  } else {
-                    final market = stocks.assetModel!;
-                    final chartData = List.generate(
-                        market.dates.length,
-                        (index) => _ChartData(
-                            market.dates[index], market.prices[index]));
-
-                    final mim = market.prices.reduce(
-                        (value, element) => value < element ? value : element);
-                    return Column(
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 5),
+                      child: Text(
+                        ticker.name,
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ),
+                    Row(
                       children: [
-                        Row(
-                          children: [
-                            TextButton(
-                                onPressed: () async {
-                                  final date = DateTime(
-                                      now.year, now.month, now.day - 7);
-                                  await stocks.getStock(market.ticker, date);
-                                },
-                                child: const Text('1W')),
-                            TextButton(
-                                onPressed: () async {
-                                  final date = DateTime(
-                                      now.year, now.month - 1, now.day);
-
-                                  await stocks.getStock(market.ticker, date);
-                                },
-                                child: const Text('1M')),
-                            TextButton(
-                                onPressed: () async {
-                                  final date = DateTime(
-                                      now.year - 1, now.month, now.day);
-
-                                  await stocks.getStock(market.ticker, date);
-                                },
-                                child: const Text('1Y')),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 300,
-                          child: SfCartesianChart(
-                            trackballBehavior: TrackballBehavior(enable: true),
-                            zoomPanBehavior: ZoomPanBehavior(
-                              enablePinching:
-                                  true, // Permite zoom com pinça (pinch-to-zoom)
-                              enablePanning: true, // Permite arrastar
-                              zoomMode:
-                                  ZoomMode.xy, // Habilita zoom nos eixos X e Y
-                            ),
-                            // title: ChartTitle(
-                            //     text: '\$${stocks.currentPrice().toStringAsFixed(2)}',
-                            //     alignment: ChartAlignment.far),
-                            tooltipBehavior: TooltipBehavior(enable: true),
-                            primaryXAxis: DateTimeAxis(
-                              intervalType: DateTimeIntervalType.months,
-                              dateFormat: DateFormat.yMd(),
-                              edgeLabelPlacement: EdgeLabelPlacement.shift,
-                            ),
-                            primaryYAxis: NumericAxis(
-                              tickPosition: TickPosition.inside,
-                              // title: AxisTitle(text: 'Preço (\$)'),
-                              minimum: mim,
-                              // interval: 1,
-                              labelFormat: '\${value}',
-                              numberFormat: NumberFormat.compact(),
-                            ),
-                            series: [
-                              LineSeries<_ChartData, DateTime>(
-                                  animationDelay: 0.0,
-                                  animationDuration: 0,
-                                  name: "price",
-                                  dataSource: chartData,
-                                  pointColorMapper: (_ChartData data, _) =>
-                                      data.price < market.prices[0]
-                                          ? Colors.red
-                                          : Colors.green,
-                                  // markerSeneSeriettings: const MarkerSettings(isVisible: true),
-                                  xValueMapper: (_ChartData data, _) =>
-                                      data.date,
-                                  yValueMapper: (_ChartData data, _) =>
-                                      data.price),
-                            ],
-                          ),
-                        ),
+                        TextButton(
+                            onPressed: () async {
+                              stocks.graph(7);
+                            },
+                            child: const Text('1W')),
+                        TextButton(
+                            onPressed: () async {
+                              stocks.graph(30);
+                            },
+                            child: const Text('1M')),
+                        TextButton(
+                            onPressed: () async {
+                              stocks.graph(365);
+                            },
+                            child: const Text('1Y')),
                       ],
-                    );
-                  }
-                },
-              ),
-            ],
+                    ),
+                    SizedBox(
+                      height: 300,
+                      child: SfCartesianChart(
+                        trackballBehavior: TrackballBehavior(enable: true),
+                        zoomPanBehavior: ZoomPanBehavior(
+                          enablePinching:
+                              true, // Permite zoom com pinça (pinch-to-zoom)
+                          enablePanning: true, // Permite arrastar
+                          zoomMode:
+                              ZoomMode.xy, // Habilita zoom nos eixos X e Y
+                        ),
+                        // title: ChartTitle(
+                        //     text: '\$${stocks.currentPrice().toStringAsFixed(2)}',
+                        //     alignment: ChartAlignment.far),
+                        tooltipBehavior: TooltipBehavior(enable: true),
+                        primaryXAxis: DateTimeAxis(
+                          intervalType: DateTimeIntervalType.months,
+                          dateFormat: DateFormat.yMd(),
+                          edgeLabelPlacement: EdgeLabelPlacement.shift,
+                        ),
+                        primaryYAxis: NumericAxis(
+                          tickPosition: TickPosition.inside,
+                          // title: AxisTitle(text: 'Preço (\$)'),
+                          minimum: mim,
+                          // interval: 1,
+                          labelFormat: '\${value}',
+                          numberFormat: NumberFormat.compact(),
+                        ),
+                        series: [
+                          LineSeries<ChartData, DateTime>(
+                              animationDelay: 0.0,
+                              animationDuration: 0,
+                              name: "price",
+                              dataSource: chartData,
+                              pointColorMapper: (ChartData data, _) =>
+                                  data.price < chartData.first.price
+                                      ? Colors.red
+                                      : Colors.green,
+                              // markerSeneSeriettings: const MarkerSettings(isVisible: true),
+                              xValueMapper: (ChartData data, _) => data.date,
+                              yValueMapper: (ChartData data, _) => data.price),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              }
+            },
           ),
         ),
       ),
     );
   }
-}
-
-class _ChartData {
-  final DateTime date;
-  final double price;
-
-  _ChartData(this.date, this.price);
 }
