13cc5a26ea13e58df5666a851b2c0e593b4c6f85
The-vinicuis <viniciusnao41@gmail.com>
2025-01-02 10:26:06 -0300

    message bitcoin

---
+++ lib/app/core/helper/percent_message.dart
@@ -0,0 +1,21 @@
+String getPriceMessage(double changePercent) {
+  if (changePercent > 0) {
+    if (changePercent <= 1) {
+      return "Leve alta! O mercado está mostrando sinais positivos.";
+    } else if (changePercent <= 5) {
+      return "Parece que algo bom está por vir!";
+    } else {
+      return "Comprar para não se arrepender!!";
+    }
+  } else if (changePercent < 0) {
+    if (changePercent >= -1) {
+      return "Leve queda. Nada fora do comum no mercado.";
+    } else if (changePercent >= -5) {
+      return "Espera cair mais um pouco para comprar!";
+    } else {
+      return "Hora de comprar! O ativo está em queda livre!";
+    }
+  } else {
+    return "Sem alterações no momento.";
+  }
+}
