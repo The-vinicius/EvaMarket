6719bf63f2a2967d70b2faa9b94e7b16aa79aac5
The-vinicuis <viniciusnao41@gmail.com>
2025-01-02 10:28:34 -0300

    inject depends pricesrepository

--- lib/app/injector.dart
+++ lib/app/injector.dart
@@ -1,6 +1,8 @@
 import 'package:auto_injector/auto_injector.dart';
 import 'package:evamarket/app/data/ticker_datasource.dart';
 import 'package:evamarket/app/repositories/data_repository.dart';
+import 'package:evamarket/app/repositories/prices_iml_repository.dart';
+import 'package:evamarket/app/repositories/prices_repository.dart';
 import 'package:evamarket/app/repositories/search_ticker_repository.dart';
 import 'package:evamarket/app/repositories/ticker_repository.dart';
 import 'package:evamarket/app/repositories/yahoo_data_repository.dart';
@@ -11,5 +13,6 @@ void setupInjector() {
   injector.add<TickerDatasource>(TickerDatasource.new);
   injector.add<TickerRepository>(SearchTickerRepository.new);
   injector.add<DataRepository>(YahooDataRepository.new);
+  injector.add<PricesRepository>(PricesImlRepository.new);
   injector.commit();
 }
